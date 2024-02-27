57924552e370c59916cfde9cbdb49d5775c1aa36
balbino05 <balbino_ivan@hotmail.com>
2024-02-27 13:53:39 -0300

    Remover componente app-tabela-dados e adicionar LoginComponent ao AppModule

--- frontEnd/testeKeevo/src/main.ts
+++ frontEnd/testeKeevo/src/main.ts
@@ -4,18 +4,20 @@ import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { TabelaDadosComponent } from './app/tabela-dados/tabela-dados.component';
+import { LoginComponent } from './app/login/login.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-  @NgModule({
-    declarations: [
-      // ... seus componentes
-      TabelaDadosComponent,
-    ],
-    imports: [
-      HttpClientModule, // Adicione esta linha
-      // ... outros módulos
-    ],
-  })
-  export class AppModule { }
+@NgModule({
+  declarations: [
+    // ... seus componentes
+    TabelaDadosComponent,
+  ],
+  imports: [
+    HttpClientModule, // Adicione esta linha
+    LoginComponent, // Move LoginComponent to the imports array
+    // ... outros módulos
+  ],
+})
+export class AppModule { }
