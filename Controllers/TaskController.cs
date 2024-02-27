18aa5ea2f08068e47b9cae82ae7e28b2e14b62e4
balbino05 <balbino_ivan@hotmail.com>
2024-02-27 20:23:22 -0300

    Atualizações no código-fonte e no arquivo package.json

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -16,6 +16,7 @@ namespace testeKeevo.Controllers
 
         public Task()
         {
+            
         }
 
         public Task( ApplicationDbContext context)
--- frontEnd/testeKeevo/src/app/tarefas/tarefas.component.ts
+++ frontEnd/testeKeevo/src/app/tarefas/tarefas.component.ts
@@ -1,33 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-tarefas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './tarefas.component.html',
   styleUrl: './tarefas.component.css'
 })
 export class TarefasComponent  implements OnInit{
-  public tarefasData: any = [{
-    id: 1,
-    title: 'Tarefa 1',
-    description: 'Descrição 1',
-    status: 'pending',
-    created_at: '2021-08-30T14:18:00.000Z',
-  },
-  {
-    id: 2,
-    title: 'Tarefa 2',
-    description: 'Descrição 2',
-    status: 'pending',
-    created_at: '2021-08-30T14:18:00.000Z',
-  },];
+  public tarefasData: any = [];
 
-  constructor() {}
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
+    this.getTarefas();
+  }
 
+  public getTarefas(): void {
+    this.http.get('https://localhost/api/task').subscribe(
+      (response : any) => { // Change the type of 'data' to 'Object'
+        this.tarefasData = response;
+      },
+      (error) => {
+        console.error('Erro na requisição:', error);
+        // Lidar com erros
+      }
+    );
+    // Implemente a lógica para buscar as tarefas
+    this.tarefasData = [
+      {
+        id: 1,
+        title: 'Tarefa 1',
+        description: 'Descrição 1',
+        status: 'pending',
+        created_at: '2021-08-30T14:18:00.000Z',
+      },
+      {
+        id: 2,
+        title: 'Tarefa 2',
+        description: 'Descrição 2',
+        status: 'pending',
+        created_at: '2021-08-30T14:18:00.000Z',
+      },
+    ];
   }
 
 }
