c70215412a1931433086b89d6e0ae90d50f50d08
balbino05 <balbino_ivan@hotmail.com>
2024-02-28 17:57:01 -0300

    Atualiza a string de conexão do banco de dados no ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,8 +6,8 @@ namespace testeKeevo.Data
 {
     public class ApplicationDbContext : DbContext
     {
-                            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-                    => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=keevo;Username=postgres;Password=Dragoon@182811");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=keevo;Username=postgres;Password=Dragoon@182811");
        public DbSet<Task> Tasks { get; set; }
     }
 
